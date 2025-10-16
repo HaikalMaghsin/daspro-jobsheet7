23e6cd095c22bd3b94d6620e90fcf8d3d505cdf1
Haikal Maghsin <haikal.maghsin0756@gmail.com>
2025-10-16 07:52:17 +0700

    SiakadLite

---
+++ SiakadFor13.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class SiakadFor13 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double nilai, tertinggi = 0,terendah = 100;
+        for (int i = 1; i <= 10; i++) {
+            System.out.print("Masukkan nilai mahasiswa ke-" + i + ": ");
+            nilai = sc.nextDouble();
+            if (nilai > tertinggi) {
+            tertinggi = nilai;
+            }
+            if (nilai < terendah) {
+            terendah = nilai;
+            }
+            
+        }
+        sc.close();
+        System.out.println("Nilai tertinggi sementara: " + tertinggi);
+        System.out.println("Nilai terendah sementara: " + terendah);
+    }
+}
